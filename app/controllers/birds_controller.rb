3dc962ae91227c345a8e67a840c9ec228a7012e4
Brian Smith <brian.bubbam@gmail.com>
2021-08-12 09:58:54 -0500

    commit tree c2c87e72a78be475d25acf338d8b1e6d7f771608

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,9 @@
 class BirdsController < ApplicationController
 
+  #instead of using rescue inside of the methods you can use rescue_from and if there are any errors in repsonses this method
+  #will automatically run
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   # GET /birds
   def index
     birds = Bird.all
@@ -14,51 +18,99 @@ class BirdsController < ApplicationController
 
   # GET /birds/:id
   def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    render json: bird
+    #if any of the below code in this method throw an error this rescue method will throw a response
+    #you can use that instead of using an if else statement
+  # rescue ActiveRecord::RecordNotFound
+  #   render_not_found_response
   end
+
+  # def show
+  #   bird = find_bird
+  #   if bird
+  #     render json: bird
+  #   else
+  #     render_not_found_response
+  #   end
+  # end
 
   # PATCH /birds/:id
   def update
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(bird_params)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(bird_params)
+    render json: bird
+    #if any of the below code in this method throw an error this rescue method will throw a response
+    #you can use that instead of using an if else statement
+  # rescue ActiveRecord::RecordNotFound
+  #   render_not_found_response
   end
+
+  # def update
+  #   bird = find_bird
+  #   if bird
+  #     bird.update(bird_params)
+  #     render json: bird
+  #   else
+  #     render_not_found_response
+  #   end
+  # end
 
   # PATCH /birds/:id/like
   def increment_likes
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.update(likes: bird.likes + 1)
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.update(likes: bird.likes + 1)
+    render json: bird
+    #if any of the below code in this method throw an error this rescue method will throw a response
+    #you can use that instead of using an if else statement
+  # rescue ActiveRecord::RecordNotFound
+  #   render_not_found_response
   end
+
+  # def increment_likes
+  #   bird = find_bird
+  #   if bird
+  #     bird.update(likes: bird.likes + 1)
+  #     render json: bird
+  #   else
+  #     render_not_found_response
+  #   end
+  # end
 
   # DELETE /birds/:id
   def destroy
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      bird.destroy
-      head :no_content
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    bird = find_bird
+    bird.destroy
+    head :no_content
+    #if any of the below code in this method throw an error this rescue method will throw a response
+    #you can use that instead of using an if else statement
+  # rescue ActiveRecord::RecordNotFound
+  #   render_not_found_response
   end
 
+  # def destroy
+  #   bird = find_bird
+  #   if bird
+  #     bird.destroy
+  #     head :no_content
+  #   else
+  #     render_not_found_response
+  #   end
+  # end
+
+  # anything that doesnt have to do with the resources routing and is a custom function is typically put as private like below
   private
 
   def bird_params
     params.permit(:name, :species, :likes)
+  end
+
+  def find_bird
+    Bird.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 
 end
